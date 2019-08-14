b0538e25cb24d211fb59253e8ad6c5834cfa0f3a
RawzaDigital <rawzadigital@gmail.com>
2019-08-14 13:33:13 +0700

    add new script on GetCurrentWeatherjobService.java

--- app/src/main/java/net/kampungweb/myjobscheduler/GetCurrentWeatherJobService.java
+++ app/src/main/java/net/kampungweb/myjobscheduler/GetCurrentWeatherJobService.java
@@ -1,10 +1,18 @@
 package net.kampungweb.myjobscheduler;
 
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -38,8 +46,8 @@ public class GetCurrentWeatherJobService extends JobService {
     private void getCurrentWeather(final JobParameters job) {
         Log.d(TAG, "Running");
         AsyncHttpClient client = new AsyncHttpClient();
-        String url = "htp://api.openweathermap.org/data/2.5/weather?q="+CITY+"appid="+APP_ID;
-        Log.e(TAG, "getCurrentWeather: "+ url);
+        String url = "htp://api.openweathermap.org/data/2.5/weather?q=" + CITY + "appid=" + APP_ID;
+        Log.e(TAG, "getCurrentWeather: " + url);
         client.get(url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
@@ -58,18 +66,43 @@ public class GetCurrentWeatherJobService extends JobService {
 
                     showNotification(getApplicationContext(), title, message, notifId);
                     jobFinished(job, false);
-                } catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+                /*
+                Ketika proses gagal, maka jobFinished di set dengan paramter true. Yang artinya job
+                perlu di reschedule
+                 */
+                jobFinished(job, true);
             }
         });
     }
 
     private void showNotification(Context context, String title, String message, int notifId) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel notificationChannel = new NotificationChannel("ID", "name", importance);
+            notificationManager.createNotificationChannel(notificationChannel);
+            builder = new NotificationCompat.Builder(context, notificationChannel.getId());
+        } else {
+            builder = new NotificationCompat.Builder(context);
+        }
+
+        builder = builder
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_replay)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.black))
+                .setVibrate(new long[]{1000,1000,1000,1000})
+                .setSound(alarmSound);
+
+        notificationManager.notify(notifId, builder.build());
     }
 }
