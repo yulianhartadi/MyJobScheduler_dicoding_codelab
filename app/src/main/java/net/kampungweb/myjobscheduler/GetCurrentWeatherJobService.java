067ca4a8ec395da0e3f42c192cd3f12bc44e318d
RawzaDigital <rawzadigital@gmail.com>
2019-08-14 12:58:57 +0700

    add API openweathermap.org and add JobService

--- app/src/main/java/net/kampungweb/myjobscheduler/GetCurrentWeatherJobService.java
+++ app/src/main/java/net/kampungweb/myjobscheduler/GetCurrentWeatherJobService.java
@@ -1,16 +1,108 @@
 package net.kampungweb.myjobscheduler;
 
+import android.app.NotificationManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+import cz.msebera.android.httpclient.Header;
 
 public class GetCurrentWeatherJobService extends JobService {
+
+    public static final String TAG = GetCurrentWeatherJobService.class.getSimpleName();
+
+    final String APP_ID = "ISIKAN DENGAN APIKEY ANDA";
+
+    //private final String CITY = "ISIKAN DENGAN NAMA KOTA ANDA";
+
+    final String CITY = "Jakarta";
+
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        return false;
+        Log.d(TAG, "onStartJob() Executed");
+        getCurrentWeather(params);
+        return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
-        return false;
+        Log.d(TAG, "onStopJob() Executed");
+        return true;
+    }
+
+    private void getCurrentWeather(final JobParameters job) {
+        Log.d(TAG, "Running");
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&appid=" + APP_ID;
+        Log.e(TAG, "getCurrentWeather: " + url);
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String result = new String(responseBody);
+                Log.d(TAG, result);
+                try {
+                    JSONObject responseObject = new JSONObject(result);
+                    String currentWeather = responseObject
+                            .getJSONArray("weather")
+                            .getJSONObject(0)
+                            .getString("main");
+                    String description = responseObject
+                            .getJSONArray("weather")
+                            .getJSONObject(0)
+                            .getString("description");
+                    double tempInKelvin = responseObject
+                            .getJSONObject("main")
+                            .getJSONObject("main")
+                            .getDouble("temp");
+                    double tempInCelcius = tempInKelvin - 273;
+                    String temperature = new DecimalFormat("##.##").format(tempInCelcius);
+                    String title = currentWeather;
+                    String message = currentWeather + ", " + description + " with " + temperature + " celcius";
+                    int notifId = 100;
+
+                    showNotification(getApplicationContext(), title, message, notifId);
+                    jobFinished(job, false);
+                } catch (Exception e) {
+                    jobFinished(job, true);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                /*
+                Ketika proses gagal, maka jobFinished di set dengan parameter true, yang artinya, job
+                perlu di reschedule
+                 */
+                jobFinished(job, true);
+
+            }
+        });
+    }
+
+    private void showNotification(Context context, String title, String message, int notifId) {
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_replay)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.black))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000})
+                .setSound(alarmSound);
+        notificationManagerCompat.notify(notifId, builder.build());
     }
 }
