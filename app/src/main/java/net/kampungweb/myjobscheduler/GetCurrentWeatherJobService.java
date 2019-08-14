dd21b336281f8a46301d528b6505ee36f01b03a7
RawzaDigital <rawzadigital@gmail.com>
2019-08-14 11:28:24 +0700

    Initial commit

---
+++ app/src/main/java/net/kampungweb/myjobscheduler/GetCurrentWeatherJobService.java
@@ -0,0 +1,16 @@
+package net.kampungweb.myjobscheduler;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+
+public class GetCurrentWeatherJobService extends JobService {
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
